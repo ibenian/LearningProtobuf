97895b4aef28f46ee382b625a97e68ca37e1965d
ilkay benian <ibenian@yahoo.com>
2017-03-11 22:17:43 -0500

    first commit

---
+++ src/main/java/step1/TestApp.java
@@ -0,0 +1,11 @@
+package step1;
+
+/**
+ * Created by ibenian on 3/11/17.
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        //step1.AddressBookProtos.Person.getDefaultInstance()
+    }
+}
