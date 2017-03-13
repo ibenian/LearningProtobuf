d400679a39da96182d25d947e4422ff4c12e84f2
ibenian <ibenian@yahoo.com>
2017-03-12 23:30:38 -0400

    RPC Service Definition

---
+++ src/main/java/step22/TestApp.java
@@ -0,0 +1,34 @@
+package step22;
+
+import com.google.protobuf.*;
+import step22.AddressBookProtos.SearchRequest;
+import step22.AddressBookProtos.SearchResponse;
+import static step22.AddressBookProtos.SearchService;
+
+
+/**
+ * Created by ibenian on 3/11/17.
+ *
+ * SearchService
+ *
+ */
+public class TestApp {
+
+    public static void main(String[] args)
+            throws InvalidProtocolBufferException {
+
+        SearchRequest.Builder builder = SearchRequest.newBuilder();
+        builder
+            .setQuery("query string ...");
+
+        SearchRequest request = builder.build();
+
+        System.out.println("request:");
+        System.out.println(request);
+
+        SearchRequest requestRead = SearchRequest.parseFrom(request.toByteArray());
+        System.out.println("request read from buffer:");
+        System.out.println(requestRead);
+
+    }
+}
